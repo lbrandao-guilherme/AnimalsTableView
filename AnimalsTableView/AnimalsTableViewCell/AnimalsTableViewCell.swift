8addb1c1cdc2cc2abd0e3a292a317420780ec219
lgsbrandao <40247614+lbrandao-guilherme@users.noreply.github.com>
2023-03-22 11:47:26 -0300

    Created Folder AnimalsTableViewCell with XIB and AnimalsTableView file. Added image and labels to XIB, made the IBOutlets and configured AnimalsTableViewCell with static identifier and NIB function

--- AnimalsTableView/AnimalsTableViewCell/AnimalsTableViewCell.swift
+++ AnimalsTableView/AnimalsTableViewCell/AnimalsTableViewCell.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 class AnimalsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var animalIconImageView: UIImageView!
+    @IBOutlet weak var animalNameLabel: UILabel!
+    @IBOutlet weak var animalYearsLabel: UILabel!
+    @IBOutlet weak var inputAnimalYearsLabel: UILabel!
+    @IBOutlet weak var animalWeightLabel: UILabel!
+    @IBOutlet weak var animalInputWeightLabel: UILabel!
+
+    //    String identifier constant
+    static let identifier: String = "AnimalsTableViewCell"
+    
+    static func setupNib() -> UINib {
+        return UINib(nibName: self.identifier, bundle: .none)
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
 }
