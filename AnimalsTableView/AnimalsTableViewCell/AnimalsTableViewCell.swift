d9872c70e5c2966fa41f523e3968b19fd06b6d51
lgsbrandao <40247614+lbrandao-guilherme@users.noreply.github.com>
2023-03-22 11:47:15 -0300

    Created Folder AnimalsTableViewCell with XIB and AnimalsTableView file. Added image and labels to XIB, made the IBOutlets and configured AnimalsTableViewCell with static identifier and NIB function

---
+++ AnimalsTableView/AnimalsTableViewCell/AnimalsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AnimalsTableViewCell.swift
+//  AnimalsTableView
+//
+//  Created by Luiz Brandão on 22/03/23.
+//
+
+import UIKit
+
+class AnimalsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
